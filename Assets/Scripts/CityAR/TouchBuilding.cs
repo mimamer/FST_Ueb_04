1b068f163d19e73179dafe5bf22f7cf46bca3e00
michelle <mersub.trier@gmail.com>
2022-12-21 17:18:31 +0100

    Metriken auf Display anzeigen

--- Assets/Scripts/CityAR/TouchBuilding.cs
+++ Assets/Scripts/CityAR/TouchBuilding.cs
@@ -17,8 +17,13 @@ namespace CityAR
         public void OnTouchStarted(HandTrackingInputEventData eventData){
             display = GameObject.Find("Display");
             var text=display.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
-            text.GetComponent<TextMeshPro>().text=entry.name;
-            gameObject.GetComponent<MeshRenderer>().material.color= new Color(Random.value, Random.value, Random.value);
+            text.GetComponent<TextMeshPro>().text=entry.name+"\n LOC: "+entry.numberOfLines+
+                                                "\n Folder: "+entry.parentEntry.name+
+                                                "\n Methods: "+entry.numberOfMethods+
+                                                "\n AbstractClasses: "+entry.numberOfAbstractClasses+
+                                                "\n Interfaces: "+entry.numberOfInterfaces;
+
+            gameObject.GetComponent<MeshRenderer>().material.color= new Color(0.8f, 0f, 0.5f);
             if(previous!=null){
                 previous.GetComponent<MeshRenderer>().material.color= new Color(1f,1f,1f);
             }
