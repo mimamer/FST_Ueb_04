9d21d5862842882b06a5ac9eb451b02913ca2cf7
michelle <mersub.trier@gmail.com>
2022-12-21 16:46:05 +0100

    change text by touch

--- Assets/Scripts/CityAR/TouchBuilding.cs
+++ Assets/Scripts/CityAR/TouchBuilding.cs
@@ -4,10 +4,15 @@ using System.Collections.Generic;
 using UnityEngine;
 using Microsoft.MixedReality.Toolkit.Input;
 using Microsoft.MixedReality.Toolkit.UI;
+using TMPro;
 
 public class TouchBuilding : MonoBehaviour, IMixedRealityTouchHandler
 {
+    private GameObject display;
     public void OnTouchStarted(HandTrackingInputEventData eventData){
+        display = GameObject.Find("Display");
+        var text=display.transform.GetChild(0).GetChild(0).GetChild(0).gameObject;
+        text.GetComponent<TextMeshPro>().text="Hallo";
         gameObject.GetComponent<MeshRenderer>().material.color= new Color(Random.value, Random.value, Random.value);
     }
 
